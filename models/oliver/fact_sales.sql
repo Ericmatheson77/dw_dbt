1cac5161ef5cf6e8ab083d993c1b0c12608fd4c3
Eric Matheson <ermguy77@gmail.com>
2025-03-20 02:58:00 +0000

    fixed fact table errors and created schema metadata

--- models/oliver/fact_sales.sql
+++ models/oliver/fact_sales.sql
@@ -7,7 +7,7 @@ SELECT
     c.customer_key,
     d.date_day,
     s.store_key,
-    ol.product_key,
+    p.product_key,
     e.employee_key,
     ol.QUANTITY,
     o.TOTAL_AMOUNT,
@@ -16,6 +16,6 @@ FROM {{ source('oliver_landing', 'orderline') }} ol
 INNER JOIN {{ source('oliver_landing', 'orders') }} o ON o.ORDER_ID = ol.ORDER_ID
 INNER JOIN {{ ref('oliver_dim_date') }} d ON o.ORDER_DATE = d.date_day
 INNER JOIN {{ ref('oliver_dim_customer') }} c ON o.CUSTOMER_ID = c.CUSTOMER_ID
-INNER JOIN {{ ref('oliver_dim_employee') }} e ON o.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN {{ ref('oliver_dim_employee') }} e ON o.EMPLOYEE_ID = e.EMPLOYEE_key
 INNER JOIN {{ ref('oliver_dim_product') }} p ON ol.PRODUCT_ID = p.PRODUCT_ID
 INNER JOIN {{ ref('oliver_dim_store') }} s ON s.STORE_ID = o.STORE_ID
